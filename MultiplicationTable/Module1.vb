dc46463a83d1a6103cfcb0024405ecae02142e09
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-14 01:42:39 +0330

    Add project files.

---
+++ MultiplicationTable/Module1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+
+Module Module1
+    Enum TableModes
+        Ten = 10
+        Hundred = 100
+        Thousand = 1000
+    End Enum
+
+    Sub Main()
+
+        System.Console.ForegroundColor = ConsoleColor.Magenta
+        System.Console.WriteLine("-------- Help Table Modes --------")
+        System.Console.ForegroundColor = ConsoleColor.Green
+        System.Console.WriteLine("1 - Ten By Ten Multiplication Table = ten")
+        System.Console.ForegroundColor = ConsoleColor.Blue
+        System.Console.WriteLine("2 - Hundred By Hundred Multiplication Table = hundred")
+        System.Console.ForegroundColor = ConsoleColor.DarkRed
+        System.Console.WriteLine("3 - Thousand By Thousand Multiplication Table = thousand")
+        System.Console.ForegroundColor = ConsoleColor.Gray
+        System.Console.WriteLine("-------------------------------------------------------")
+        System.Console.ForegroundColor = ConsoleColor.Cyan
+        System.Console.Write("Enter the table type : ")
+        System.Console.ForegroundColor = ConsoleColor.Yellow
+        Dim mode As String = System.Console.ReadLine().ToLower()
+        Dim number As Integer = 10
+
+        If mode = "ten" Then
+            number = TableModes.Ten
+        ElseIf mode = "hundred" Then
+            number = TableModes.Hundred
+        ElseIf mode = "thousand" Then
+            number = TableModes.Thousand
+        End If
+        System.Console.ForegroundColor = ConsoleColor.Gray
+        System.Console.WriteLine("-------------------------------------------------------")
+
+        System.Console.ForegroundColor = ConsoleColor.Black
+        System.Console.BackgroundColor = ConsoleColor.Gray
+        For i As Integer = 1 To number
+
+            For j As Integer = 1 To number
+                System.Console.Write(i * j & vbTab)
+            Next
+
+            System.Console.WriteLine()
+        Next
+        System.Console.BackgroundColor = ConsoleColor.Black
+        System.Console.ForegroundColor = ConsoleColor.Magenta
+        System.Console.WriteLine("-----------------------")
+        System.Console.ForegroundColor = ConsoleColor.Red
+        System.Console.WriteLine("Programmer : Amin Mirzaei - Github(UserName) : AminMirzaeiOne")
+        System.Console.ReadKey()
+    End Sub
+
+End Module
---
+++ MultiplicationTable/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MultiplicationTable")>
+<Assembly: AssemblyDescription("Among the first projects of Amin Mirzaei after learning the basics of Visual Basic.NET")>
+<Assembly: AssemblyCompany("AminMirzaeiOne")>
+<Assembly: AssemblyProduct("MultiplicationTable")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("AminMirzaeiOne")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("17277c97-dd51-43ce-9f04-3e5397e1ec41")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
